6aa71729e51ee5bd5dee888aef598590dffbd954
Al Mamun <wdmamun75@gmail.com>
2023-08-20 16:42:52 +0600

    js finished commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-function addToCard(){
+function addToCard() {
     const itemPrice = document.getElementById('item-price').innerText;
     const displayContainer = document.getElementById('display-container');
     const countNumber = displayContainer.childElementCount;
@@ -8,7 +8,7 @@ function addToCard(){
 
     const p = document.createElement('p');
     p.innerText = `${countNumber + 1}. ${itemName}`;
-    p.classList.add('font-semibold' , 'text-lg' ,'text-[#111]' );
+    p.classList.add('font-semibold', 'text-lg', 'text-[#111]');
 
     displayContainer.appendChild(p);
 
@@ -19,21 +19,45 @@ function addToCard(){
     const updateTotalPrice = parseFloat(displayTotalPrice.innerText) + parseFloat(itemPrice);
     displayTotalPrice.innerText = updateTotalPrice.toFixed(2);
 
-    
+
 
     // purchase button 
-    if(displayTotalPrice.innerText > 0){
+    if (displayTotalPrice.innerText > 0) {
         const purchaseButton = document.getElementById('purchase-button');
-        purchaseButton.classList.remove('pointer-events-none' , 'opacity-[50%]');
+        purchaseButton.classList.remove('pointer-events-none', 'opacity-[50%]');
     }
 
     // apply button
-    if(displayTotalPrice.innerText >= 200){
+    if (displayTotalPrice.innerText >= 200) {
         const applyButton = document.getElementById('apply-button');
-        applyButton.classList.remove('pointer-events-none' , 'opacity-[50%]');
+        applyButton.classList.remove('pointer-events-none', 'opacity-[50%]');
     }
 
 }
+
+
+document.getElementById('apply-button').addEventListener('click', function () {
+    
+    const displayTotalPrice = document.getElementById('display-total-price');
+
+    const couponInput = document.getElementById('coupon-input');
+    const couponInputValue = couponInput.value;
+    
+
+    if(couponInputValue === 'SELL200'){
+
+        const discountElement = document.getElementById('discount-element');
+        const lessTotalDisplay = document.getElementById('less-total-price');
+
+        const discountAmount = (displayTotalPrice.innerText / 100) * 20;
+        discountElement.innerText = discountAmount.toFixed(2);
+
+        const lessTotalAmount = displayTotalPrice.innerText - discountAmount;
+        lessTotalDisplay.innerText = lessTotalAmount.toFixed(2);
+
+    }
+
+});
 
 
 
